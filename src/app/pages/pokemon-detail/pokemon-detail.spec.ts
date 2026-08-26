32297b5abfae91bb456f9a38e2a2270329e008bc
Killian OCTAU <killoctau@gmail.com>
2026-08-26 16:44:52 +0200

    page detail avec types et barres de statistiques

--- src/app/pages/pokemon-detail/pokemon-detail.spec.ts
+++ src/app/pages/pokemon-detail/pokemon-detail.spec.ts
@@ -1,4 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { provideHttpClient } from '@angular/common/http';
+import { provideRouter } from '@angular/router';
 
 import { PokemonDetailPage } from './pokemon-detail';
 
@@ -9,6 +11,7 @@ describe('PokemonDetailPage', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [PokemonDetailPage],
+      providers: [provideHttpClient(), provideRouter([])],
     }).compileComponents();
 
     fixture = TestBed.createComponent(PokemonDetailPage);
--- src/app/pages/pokemon-detail/pokemon-detail.ts
+++ src/app/pages/pokemon-detail/pokemon-detail.ts
@@ -1,9 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject, signal } from '@angular/core';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { PokemonService } from '../../services/pokemon';
+import { PokemonDetail } from '../../models/pokemon.model';
 
 @Component({
   selector: 'app-pokemon-detail',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './pokemon-detail.html',
   styleUrl: './pokemon-detail.css',
 })
-export class PokemonDetailPage {}
+export class PokemonDetailPage implements OnInit {
+  private route = inject(ActivatedRoute);
+  private pokemonService = inject(PokemonService);
+
+  loading = signal(true);
+  error = signal(false);
+  pokemon = signal<PokemonDetail | null>(null);
+
+  ngOnInit(): void {
+    // On recupere l'id depuis l'URL /pokemon/:id
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+
+    this.pokemonService.getPokemonDetail(id).subscribe({
+      next: (detail) => {
+        this.pokemon.set(detail);
+        this.loading.set(false);
+      },
+      error: () => {
+        this.error.set(true);
+        this.loading.set(false);
+      },
+    });
+  }
+}
